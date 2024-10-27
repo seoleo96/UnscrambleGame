eb3e3f18b4bc040aa977b9c060e3162baddaf217
sardoriiiy <s.sarimsakov@indev.expert>
2024-10-27 21:19:10 +0500

    В MainActivity были написаны базовые шаги приложения

--- app/src/main/java/com/seoleo/unscramblegame/MainActivity.kt
+++ app/src/main/java/com/seoleo/unscramblegame/MainActivity.kt
@@ -1,25 +1,52 @@
 package com.seoleo.unscramblegame
 
 import android.os.Bundle
+import android.text.Editable
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.addTextChangedListener
 import com.seoleo.unscramblegame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var viewBinding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        viewBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(viewBinding.root)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.rootLayout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val viewModel = GameViewModel()
+
+        binding.nextButton.setOnClickListener {
+            val uiState: GameUiState = viewModel.next()
+            uiState.update(binding = binding)
+        }
+
+        binding.skipButton.setOnClickListener {
+            val uiState: GameUiState = viewModel.skip()
+            uiState.update(binding = binding)
+        }
+
+        binding.checkButton.setOnClickListener {
+            val uiState: GameUiState = viewModel.check(text = binding.inputEditText.text.toString())
+            uiState.update(binding = binding)
+        }
+
+        binding.inputEditText.addTextChangedListener { text: Editable? ->
+            val uiState: GameUiState = viewModel.handleUserInput(text = text.toString())
+            uiState.update(binding = binding)
+        }
+
+        val uiState = viewModel.init()
+        uiState.update(binding = binding)
     }
 }
